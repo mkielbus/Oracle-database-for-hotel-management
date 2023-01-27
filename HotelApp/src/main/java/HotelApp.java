77c405c1aafebcfaf3583b2b1c755ad9cd2febe9
Alicja Kubiszyn <01169199@pw.edu.pl>
2023-01-27 18:24:52 +0100

    added function to calculate total cost and to add extra service

--- HotelApp/src/main/java/HotelApp.java
+++ HotelApp/src/main/java/HotelApp.java
@@ -2,6 +2,8 @@
 import java.io.FileInputStream;
 import java.io.*;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 import oracle.jdbc.pool.OracleDataSource;
@@ -56,16 +58,78 @@ public class HotelApp {
     }
 
     public void mainWindow() throws SQLException {
-        System.out.println("Wybierz polecenie: \nWyświetl gości: 1 \nDodaj gościa: 2");
-        Scanner in = new Scanner(System.in);
-        String request = in.nextLine();
-        System.out.println("Wybrałeś polecenie " + request);
-        switch (request){
-            case "1":
-                this.showGuests();
-            case "2":
-                this.addGuest();
+        while(true) {
+            System.out.println("--------------------------");
+            System.out.println("Wybierz polecenie: \nWyświetl gości: 1 \nDodaj gościa: 2 \nOblicz koszt pobytu: 3 \nDodaj dodatkową usługę: 4");
+            Scanner in = new Scanner(System.in);
+            String request = in.nextLine();
+            System.out.println("Wybrałeś polecenie " + request);
+            switch (request) {
+                case "1":
+                    this.showGuests();
+                    break;
+                case "2":
+                    this.addGuest();
+                    break;
+                case "3":
+                    this.calculateCost();
+                    break;
+                case "4":
+                    this.addExtraService();
+            }
         }
+    }
+
+    private String getIdGuest() throws SQLException{
+        Statement statement = conn.createStatement();
+        System.out.println("Wpisz numer id gościa:");
+        Scanner in = new Scanner(System.in);
+        String guest_id = in.nextLine();
+        return guest_id;
+    }
+
+    private void showList(ArrayList<String> listOfWords) throws SQLException{
+        Statement statement = conn.createStatement();
+        System.out.println("Wybierz " + listOfWords.get(0)+ ", wprowadzając " + listOfWords.get(1) + " numer:  ");
+        ResultSet rs = statement.executeQuery(listOfWords.get(2));
+        while(rs.next()){
+            String first = rs.getString(1);
+            String second = rs.getString(2);
+            String third = rs.getString(3);
+            System.out.println(listOfWords.get(3) + first + listOfWords.get(4) +second + listOfWords.get(5) + third);
+
+        }
+    }
+    private void addExtraService() throws SQLException {
+        Scanner scanner = new Scanner(System.in);
+        PreparedStatement preparedStatement = conn.prepareStatement("INSERT INTO guest_service VALUES (?, ?, ?)");
+        String guest_id = this.getIdGuest();
+        ArrayList<String> listOfWords = new ArrayList<String>(Arrays.asList("usługę", "jej", "SELECT * FROM extra_service", "Numer serwisu: ", ", nazwa: ", ", koszt: " ));
+        showList(listOfWords);
+        String service_id = scanner.nextLine();
+        System.out.println("Wybierz ilość: ");
+        String quantity = scanner.nextLine();
+        preparedStatement.setString(1, service_id);
+        preparedStatement.setString(2, guest_id);
+        preparedStatement.setString(3, quantity);
+        int executedUpdate = preparedStatement.executeUpdate();
+        System.out.println("Dodano usługę.");
+
+
+
+    }
+
+    private void calculateCost() throws SQLException{
+        String guest_id = this.getIdGuest();
+        PreparedStatement preparedStatement = conn.prepareStatement("select re.stay_length * ro.cost + nvl(g.quantity * es.cost, 0) from reservations re join rooms ro using(room_id) left join guest_service g on(re.guest_id = g.guest_id) left join extra_service es on(es.service_id = g.service_id)  where re.guest_id = ?");
+        preparedStatement.setString(1, guest_id);
+        ResultSet rs = preparedStatement.executeQuery();
+        String cost = "";
+        while(rs.next()){
+            cost = rs.getString(1);
+        }
+        System.out.println("Koszt pobytu jest równy: " + cost);
+
     }
 
     private void addGuest() throws SQLException{
@@ -75,15 +139,8 @@ public class HotelApp {
         String name = in.nextLine();
         System.out.println("Wprowadź nazwisko: ");
         String surname = in.nextLine();
-        System.out.println("Wybierz pokój, wprowadzając jego numer:  ");
-        ResultSet rs = statement.executeQuery("SELECT * FROM rooms WHERE occupied = 0");
-        while(rs.next()){
-            String room_id = rs.getString(1);
-            String capacity = rs.getString(2);
-            String cost = rs.getString(3);
-            System.out.println("Numer pokoju: " + room_id + ", pojemność: " + capacity + ", koszt za noc: " + cost);
-
-        }
+        ArrayList<String> listOfWords = new ArrayList<String>(Arrays.asList("pokój", "jego", "SELECT * FROM rooms WHERE occupied = 0", "Numer pokoju: ", ", pojemność: ", ", koszt za noc: " ));
+        showList(listOfWords);
         String room_id = in.nextLine();
         System.out.println("Wpisz ilość dni: ");
         String stay_length = in.nextLine();
@@ -108,6 +165,7 @@ public class HotelApp {
         preparedStatement1.setString(3, guest_id);
         preparedStatement1.setString(4, stay_length);
         int executed2 = preparedStatement1.executeUpdate();
+        System.out.println("Dodano gościa");
 
 
 
